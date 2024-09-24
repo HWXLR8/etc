bbc11c1cc57b9e8cff9421ad7c66d84dcd161b16
HWXLR8 <68k@segv.moe>
2024-09-24 19:49:36 -0400

    [ARCH] add core package installation script

---
+++ arch/install-core.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo pacman -S \
+     xorg-xserver \
+     xorg-xinit \
+     xorg-xrandr \
+     i3-gaps \
+     i3status \
+     rxvt-unicode \
+     terminus-font \
+     feh \
+     emacs-nox \
+     which \
+     less \
+     htop
