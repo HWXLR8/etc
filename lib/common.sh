a91382c17ae977162e7894bf25d833c563b1ced9
HWXLR8 <68k@segv.moe>
2024-09-29 01:27:40 -0400

    [alsa][lib] add check_param to exit if no param supplied

--- lib/common.sh
+++ lib/common.sh
@@ -15,3 +15,11 @@ function force_root {
 function append_xinit {
     sed -i "/^exec i3/c\\$1\nexec i3" ~/.xinitrc
 }
+
+function check_param {
+    if [ -z ${1+x} ];
+    then
+	log "no argument supplied"
+	exit
+    fi
+}
