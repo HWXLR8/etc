fcc22f358d27ae4bf2190817816977a035763635
HWXLR8 <68k@segv.moe>
2024-11-19 10:13:01 -0500

    [alias] add ds to stop all docker containers

--- lib/alias/docker.sh
+++ lib/alias/docker.sh
@@ -1,5 +1,6 @@
 # docker
 alias drm_all='docker rm -vf $(docker ps -a -q) && docker rmi -f $(docker images -a -q)'
+alias ds='docker stop $(docker ps -q)'
 alias dc='docker container'
 alias di='docker image'
 alias dv='docker volume'
