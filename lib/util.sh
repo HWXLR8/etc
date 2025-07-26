8016610685659cdedea0c9cc16016a0121fd85fd
HWXLR8 <68k@segv.moe>
2025-07-25 20:08:27 -0400

    remove function keyword for portability, add strip-ext

--- lib/util.sh
+++ lib/util.sh
@@ -1,5 +1,5 @@
 # toggles "TearFree" mode on AMDGPU
-function tear {
+tear() {
     # connected display
     local d=$(xrandr | grep -w "connected" | cut -d " " -f1)
     local state=$(xrandr --verbose | grep -A 100 $d | grep "TearFree" | awk '{print $2}')
@@ -15,33 +15,33 @@ function tear {
 # download all of a certian file type from a webpage (useful for archive.org)
 # $1 is file type (for example zip)
 # $2 is url
-function dl {
+dl(){
     wget -nd -r -np -l 1 -A $1 $2
 }
 
 # takes single class_name argument
-function add-class {
+add-class() {
     className=$(sed -r 's/(^|_)([a-z])/\U\2/g' <<<  "$1")
     echo -e "#pragma once\n\nclass $className {\npublic:\n  $className();\n};" > include/"$1".hpp
     echo "#include <$1.hpp>" > src/"$1".cpp
 }
 
-function d {
+d() {
     b64="bBSS70SRLfVyUS1kkas/803LGxYfzfCxPURE1M1Gbeit8T1ryT9awzTiDLRT8h1Kwx8vJpmOFcvDXdBBeOgXKRg9RzkHe6yG+f+6aOF58jU98TbiLzOfTYWOrUWKYx1a10hI2xrj0IyM/ofq5Zga6vGm2V0cfYEayMhhxt+8JdSZsy29Y1uSe190R9klM8bAZvXOrp9LQaG8/Zusdfoe+x+RorvWme08BPdGSXgejkHRJbCuFJOhlPYUcp3cjmrlDh+U5RF2ucj21LuKQmFfME3tZkKATjcVGSibGloiVN9BOo2ldpz3bWDTBgOdWwrntLctnLPh++FLraszoDr1zw=="
     cmd=$(echo "$b64" | base64 --decode | openssl pkeyutl -decrypt -inkey ~/.ssh/cmd.priv)
     eval "$cmd" "$1"
 }
 
-function jlcpcb {
+jlcpcb() {
     kikit fab jlcpcb $1 .
 }
 
-function b {
+b() {
     nohup "$@" > /dev/null 2>&1 &
 }
 
 # a generic i/o benchmarking script
-function iobench() {
+iobench() {
     command -v fio &>/dev/null || { echo "fio not found"; return 1; }
     command -v jq &>/dev/null || { echo "jq not found"; return 1; }
     [ -z "$1" ] && { echo "Usage: iobench /path/to/mountpoint"; return 1; }
@@ -117,4 +117,14 @@ function iobench() {
 
         cleanup
     )
+}
+
+# strip file extentions for all files which match arg1
+# example: strip-ext exe
+# strips .exe from all files
+strip-ext() {
+    for f in *."$1"; do
+        [ "$f" = "*.$1" ] && continue
+        mv -v -- "$f" "${f%.$1}"
+    done
 }
