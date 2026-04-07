993755c34e263d5fd8d407b5a55da53afaea7e86
HWXLR8 <68k@segv.moe>
2026-04-07 01:00:36 -0400

    update bashrc env variables

--- lib/env.sh
+++ lib/env.sh
@@ -1,5 +1,17 @@
 export EDITOR='emacs -nw'
+export TERM='xterm'
 export PS1='\[\e[0;35m\][\W]\[\e[0m\]$(__git_ps1) '
 
 # for qt5 moc used by MAME
 export PATH=$PATH:/usr/lib64/qt5/bin
+
+# libxenon
+export DEVKITXENON="/usr/local/xenon"
+export PATH="${PATH:+${PATH}:}"$DEVKITXENON"/bin:"$DEVKITXENON"/usr/bin"
+
+# claude
+export ANTHROPIC_BASE_URL="http://localhost:8001"
+export ANTHROPIC_API_KEY='sk-ant-local-123'
+
+# theme
+export QT_STYLE_OVERRIDE=Adwaita-Dark
